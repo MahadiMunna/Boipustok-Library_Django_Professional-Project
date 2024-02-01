e97f3280ad08161c968f2fbfd6de84564f10ac35
MahadiMunna <mahadimunna.official@gmail.com>
2024-02-01 23:37:09 +0600

    books model and category added

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookModel, BookCategory
 
 # Register your models here.
+admin.site.register(BookModel)
+admin.site.register(BookCategory)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class BookCategory(models.Model):
+    category_name = models.CharField(max_length=20)
+    
+    def __str__(self) -> str:
+        return self.category_name
+    
+class BookModel(models.Model):
+    book_title = models.CharField(max_length=200)
+    book_description = models.TextField()
+    book_category = models.ManyToManyField(BookCategory)
+    book_image = models.ImageField(upload_to='./books/uploads/', blank=True, null=True)
+    price = models.DecimalField(default=0, max_digits=12, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.book_title
+
